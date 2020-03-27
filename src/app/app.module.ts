57f7ead0c5d52122b94c85ece55701bad4dccd68
davirdnet <davirdnet@gmail.com>
2020-03-26 22:23:42 -0300

    criação do projeto e serviço

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { EditComponent } from './contatos/edit/edit.component';
+import { ListComponent } from './contatos/list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EditComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAiPpbgNpyN0-XSdmDXrQOzZXCSm5FBIAw",
+    authDomain: "crud-angular-davirdnet.firebaseapp.com",
+    databaseURL: "https://crud-angular-davirdnet.firebaseio.com",
+    projectId: "crud-angular-davirdnet",
+    storageBucket: "crud-angular-davirdnet.appspot.com",
+    messagingSenderId: "778082354006",
+    appId: "1:778082354006:web:1340e35c14077bc8ec4cf7",
+    measurementId: "G-MFEM4R8DYT"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
